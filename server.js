f877d45aa280b4a19ac5f6bef6bf7d94754a1129
Japstine <jeetmehulpatel@gmail.com>
2025-03-08 15:28:24 +0530

    Learning the request and response

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = 7000;
+const port = process.env.PORT || 7000;
+
+app.get('/api/contacts', (req, res) => {
+  res.json("Get all contacts");
+})
 
 app.listen(port, () => {
   // Use `` for log (to pass a variable)
